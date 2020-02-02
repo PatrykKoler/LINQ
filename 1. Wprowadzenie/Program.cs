d479c33e3de250f7445d932ed4d164b1f4903a26
Patryk <koler.patryk13@gmail.com>
2020-02-02 14:49:40 +0100

    Wyprowadzania plikow bez LINQu

--- 1. Wprowadzenie/Program.cs
+++ 1. Wprowadzenie/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace _1.Wprowadzenie
     {
         static void Main(string[] args)
         {
+            var sciezka = @"C:\windows";
+            PokazPlikiBezLinqu(sciezka);
+        }
+
+        private static void PokazPlikiBezLinqu(string sciezka)
+        {
+            DirectoryInfo katalog = new DirectoryInfo(sciezka);
+            FileInfo[] pliki = katalog.GetFiles();
+
+            Array.Sort(pliki, new FileInfoComparer());
+            for (int i = 0; i < 5; i++ )
+            {
+                FileInfo plik = pliki[i];
+                Console.WriteLine($"{plik.Name, -20} : {plik.Length, 15:N0}");
+            }
         }
     }
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+
 }
